159ee5c2bbfdfd86c15f1001be9ce2cf99627fdd
ProjetosStephaniemota15 <100208643+ProjetosStephaniemota15@users.noreply.github.com>
2023-03-14 12:39:21 -0300

    Desafio

--- ExercicioAlunos/Program.cs
+++ ExercicioAlunos/Program.cs
@@ -3,11 +3,19 @@ class Program
 {
     static void Main(string[] args)
     {
-       Alunos aluno1 = new Alunos();
-       aluno1.nome = "Outro Aluno";
-       aluno1.nota1 = 4;
-       aluno1.nota2 = 5;
+        Alunos aluno1 = new Alunos();
+        Console.WriteLine("Digite o nome do aluno: ");
+        aluno1.nome = Console.ReadLine(); 
 
-       aluno1.mensagem();
+
+        Console.WriteLine("Digite a primeira nota: ");
+        aluno1.nota1 = double.Parse(Console.ReadLine());
+
+       
+        Console.WriteLine("Digite a segunda nota: ");
+        aluno1.nota2 = double.Parse(Console.ReadLine());
+        
+
+        aluno1.mensagem();
     }
 }
