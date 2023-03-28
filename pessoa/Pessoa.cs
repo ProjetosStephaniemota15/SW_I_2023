827d12d673c6a44bf12847a1af3d3f8dbb3ef031
ProjetosStephaniemota15 <100208643+ProjetosStephaniemota15@users.noreply.github.com>
2023-03-28 11:58:45 -0300

    Add files via upload

---
+++ pessoa/Pessoa.cs
@@ -0,0 +1,39 @@
+﻿namespace pessoa;
+class Pessoa
+{
+   
+   public Pessoa(){
+    this.Nome = "Não Informado";
+    this.Sobrenome = "Não Informado";
+    this.Idade = 0;
+   }
+
+   public Pessoa(string nome, int idade){
+    this.Nome = nome;
+    this.Sobrenome = "Não Informado";
+    this.Idade = idade;
+   }
+
+   private string? Nome {get;set;}
+   private string? Sobrenome {get;set;}
+   private int Idade {get;set;}
+
+   public void AlteraNome(string nome){
+    this.Nome = nome;
+   }
+
+   public void AlteraSobrenome(string sobrenome){
+    this.Sobrenome = sobrenome;
+   }
+
+   public void AlteraIdade(int idade){
+    this.Idade = idade;
+   }
+
+   public void MostraDados(){
+    Console.WriteLine("Nome: " + this.Nome);
+    Console.WriteLine("Sobrenome: " + this.Sobrenome);
+    Console.WriteLine("Idade: " + this.Idade);
+   }
+   
+}
---
+++ pessoa/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("Stephanie" ,16);
+        //p1.AlteraNome("Stephanie");
+        //p1.AlteraSobrenome("Mota");
+        //p1.AlteraIdade(16);
+        p1.MostraDados();
+    }
+}
